76cb511e6a108e3fa973b6737edf22869afc341c
Allison Irvin <allison.irvin2@gmail.com>
2021-01-30 11:53:54 +1100

    Completed prompt state machine

--- src/main/kotlin/search/cli/PromptOptions.kt
+++ src/main/kotlin/search/cli/PromptOptions.kt
@@ -3,41 +3,31 @@ package search.cli
 import com.github.ajalt.clikt.output.TermUi
 import kotlin.reflect.full.memberProperties
 
-class PromptOptions {
-    companion object {
-
-        val INTRODUCTION = "Welcome to Zendesk Search!\n" +
-                "Type 'ctrl+c' at any time to exit\n"
-
-        val INTRO_SELECTION = "       Select search option:\n" +
-                "       * Press 1 to search Zendesk\n" +
-                "       * Press 2 to view searchable fields\n"
-
-        val ENTITY_SELECTION = "Select entity to search \n${Entity.values.mapIndexed {i, it -> "${i+1}) $it\n"}
-                .reduce { acc, it -> acc + it }}"
-
-        val ENTITY_SEARCH_OPTIONS = "Enter search term or hit enter to view fields for "
-
-    }
-}
-
-
 fun promptStateMachineInit() {
     val introSelection = getIntroSelection()
     val entitySelection = getEntitySelection(introSelection == 2)
     handleSearchOptions(entitySelection)
 }
 
-fun handleSearchOptions(entitySelection: Entity) {
+fun handleSearchOptions(entitySelection: String) {
     val searchTerm = getSearchTerm(entitySelection)
     val searchValue = getSearchValue(searchTerm, entitySelection) 
-    val resetSelection = getResults(searchValue, searchTerm, entitySelection)
-    if (resetSelection == 1) promptStateMachineInit() else handleSearchOptions(entitySelection)
+    val results = getResults(searchValue, searchTerm, entitySelection)
+    println(results)
+    val promptResetSelection = getResetSelection(entitySelection)
+    if (promptResetSelection == 1) handleSearchOptions(entitySelection) else promptStateMachineInit()
 }
 
 fun getIntroSelection(init: Boolean = true): Int {
-    val promptText = if (init) PromptOptions.INTRODUCTION else ""
-    val initialSelection = TermUi.prompt(promptText + PromptOptions.INTRO_SELECTION)
+    val introduction = "Welcome to Zendesk Search!\n" +
+            "Type 'ctrl+c' at any time to exit\n"
+
+    val intro_selection = "       Select search option:\n" +
+            "       * Press 1 to search Zendesk\n" +
+            "       * Press 2 to view searchable fields\n"
+
+    val introText = if (init) introduction else ""
+    val initialSelection = TermUi.prompt(introText + intro_selection)
 
     return if (initialSelection == null || initialSelection.toIntOrNull() == null ) {
         getIntroSelection(false)
@@ -46,14 +36,16 @@ fun getIntroSelection(init: Boolean = true): Int {
     }
 }
 
-fun getEntitySelection(displayFields: Boolean): Entity {
-    val promptText = if (displayFields) {
+fun getEntitySelection(displayFields: Boolean): String {
+    val entitySelectionText = "Select entity to search \n${Entity.values.mapIndexed {i, it -> "${i+1}) $it\n"}
+            .reduce { acc, it -> acc + it }}"
+
+    val entityFields = if (displayFields) {
         Entity.values.map { printSearchableFields(it) }
-                .reduce {acc, it -> acc + it} + "\n" + PromptOptions.ENTITY_SELECTION
-    } else {
-        PromptOptions.ENTITY_SELECTION
-    }
-    val entitySelection = TermUi.prompt(promptText)
+                .reduce {acc, it -> acc + it} + "\n"
+    } else ""
+
+    val entitySelection = TermUi.prompt(entityFields + entitySelectionText)
 
     return if (entitySelection == null
             || entitySelection.toIntOrNull() == null
@@ -62,38 +54,52 @@ fun getEntitySelection(displayFields: Boolean): Entity {
     ) {
         getEntitySelection(false)
     } else {
-        Entity.getByInt(entitySelection.toInt())!!
+        Entity.getByInt(entitySelection.toInt())!!.toString().toLowerCase().capitalize()
     }
 }
 
-fun getSearchTerm(entityType: Entity): String {
-    val searchTerm = TermUi.prompt(PromptOptions.ENTITY_SEARCH_OPTIONS + entityType.toString().toLowerCase().capitalize())
-    return if (searchTerm == null || !isValidSearchTerm(searchTerm, entityType)) {
-        // TODO: display fields for entityType
-        getSearchTerm(entityType)
+fun getSearchTerm(entity: String, displayFields: Boolean = false): String {
+    val entityFields = if (displayFields) printSearchableFields(entity) else ""
+    val searchTerm = TermUi.prompt("${entityFields}Enter search term for $entity", "")
+    return if (searchTerm.isNullOrEmpty() || !isValidSearchTerm(searchTerm, entity)) {
+        getSearchTerm(entity, true)
     } else {
         searchTerm
     }
 }
 
-fun getSearchValue(searchTerm: String, entitySelection: Entity): String = "TODO"
+fun getSearchValue(searchTerm: String, entity: String): String {
+    val searchValue = TermUi.prompt("Enter search value for $entity $searchTerm")
+    return if (searchValue.isNullOrEmpty()) getSearchValue(searchTerm, entity) else searchValue
+}
 
-fun getResults(searchValue: String, searchTerm: String, entitySelection: Entity): Int = 1
+fun getResults(searchValue: String, searchTerm: String, entity: String): String
+    = "Results for searching $entity $searchTerm $searchValue"
+
+fun getResetSelection(entity: String): Int {
+    val resetSelection = TermUi.prompt("Enter 1 to search $entity again, or 2 to restart from the start")
+    return if (resetSelection == null || resetSelection.toIntOrNull() == null) {
+        getResetSelection(entity)
+    } else {
+        resetSelection.toInt()
+    }
+}
 
 /* In order to make this application extensible, the property names that can be
 searched on should not be hard coded.
 Here, reflection is used to analyse the properties of the classes to create the
 list of searchable fields without having to hardcode any of them.
  */
-fun printSearchableFields(entityType: String): String {
+fun printSearchableFields(entity: String): String {
     val header = "--------------------------------------\n" +
-            "Search ${entityType}s with\n"
+            "Search ${entity}s with\n"
 
-    val entityProperties = Class.forName("search.cli.$entityType").kotlin.memberProperties
+    val entityProperties = Class.forName("search.cli.$entity").kotlin.memberProperties
             .map { it.name }
             .reduce {acc, it -> acc + "\n" + it}
 
     return "$header $entityProperties \n"
 }
 
-fun isValidSearchTerm(searchTerm: String, entityType: Entity): Boolean = true
+fun isValidSearchTerm(searchTerm: String, entity: String): Boolean =
+     Class.forName("search.cli.$entity").kotlin.memberProperties.map { it.name }.contains(searchTerm)
